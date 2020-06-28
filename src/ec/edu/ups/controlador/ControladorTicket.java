c81b0ca45f31c7508985cfc044c897fd8f466f5c
Sebastian Uyaguari <>
2020-06-27 22:42:25 -0500

    Creacion de la clase controlador Ticket

---
+++ src/ec/edu/ups/controlador/ControladorTicket.java
@@ -0,0 +1,74 @@
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.idao.ITicketDAO;
+import ec.edu.ups.modelo.Ticket;
+import java.util.Date;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ControladorTicket {
+    
+    private ITicketDAO ticketDAO;
+
+    private Ticket ticket;
+
+    public ControladorTicket(ITicketDAO ticketDAO) {
+        this.ticketDAO = ticketDAO;
+    }
+    
+   public void crearTicket(int numero, Date ingreso){
+       
+       ticket = new Ticket(numero, ingreso);
+       ticketDAO.create(ticket);
+       
+   }
+   
+   public Ticket buscarTicket(int numero){
+       
+       ticket = ticketDAO.read(numero);
+       
+       if(ticket!= null){
+           return ticket;
+       }else{
+           return null;
+       }
+   }
+   
+   public void actualizarTicket(int numero, Date ingreso, Date salida, int fracciones, double total){
+       
+       ticket= new Ticket(numero, ingreso, salida, fracciones, total);
+       
+       ticketDAO.update(ticket);
+       
+   }
+   
+   public void eliminarTicket(Ticket ticket){
+       
+       ticketDAO.delite(ticket);
+       
+   }
+   
+   public double calcularTotal(Date ingreso, Date salida){
+       
+       double total = ticket.calcularTotal(ingreso, salida);
+       return total;
+       
+   }
+   
+   public int calcularFracciones(double total){
+       
+       int fracciones = ticket.calcularFracciones(total);
+       return fracciones;
+       
+   }
+   
+   public int codigo(){
+       
+       int codigo = ticketDAO.codigo();
+       return codigo++ ;
+   
+   }
+    
+}
