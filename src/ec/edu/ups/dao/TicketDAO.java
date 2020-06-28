0b178d15cd6bc8fac4f4eb88cebc8ec7e3ae9941
Santiago-cabrera-Arias <santiagocabrera354@gmail.com>
2020-06-27 22:33:54 -0500

    clase TicketDAO que contiene los metodos sobreescritos ITicketDAO

---
+++ src/ec/edu/ups/dao/TicketDAO.java
@@ -0,0 +1,65 @@
+package ec.edu.ups.dao;
+
+import ec.edu.ups.idao.ITicketDAO;
+import ec.edu.ups.modelo.Ticket;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class TicketDAO implements ITicketDAO{
+    
+    private Map<Integer, Ticket> listaTickets;
+    private int codigo;
+
+    public TicketDAO() {
+    
+        listaTickets = new HashMap<>();
+        codigo=0;
+        
+    }    
+    
+
+    @Override
+    public void create(Ticket ticket) {
+        
+        listaTickets.put(ticket.getNumero(), ticket);
+        
+    }
+
+    @Override
+    public Ticket read(int numero) {
+        
+        return listaTickets.get(numero);
+        
+    }
+
+    @Override
+    public void update(Ticket ticket) {
+        
+        listaTickets.put(ticket.getNumero(), ticket);
+        
+    }
+
+    @Override
+    public void delite(Ticket ticket) {
+        
+        listaTickets.remove(ticket.getNumero());
+        
+    }
+
+    @Override
+    public Map<Integer, Ticket> findAll() {
+        
+        return listaTickets;
+                
+    }
+
+    @Override
+    public int codigo() {
+        return codigo;
+    }
+    
+}
