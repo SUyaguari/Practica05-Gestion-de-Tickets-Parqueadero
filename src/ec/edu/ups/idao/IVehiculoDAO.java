5de5ec9e9971a1e28e282b0effd3c1107a767dcb
Santiago-cabrera-Arias <santiagocabrera354@gmail.com>
2020-06-27 22:16:49 -0500

    Creacion de la clase IVehiculoDAO.

---
+++ src/ec/edu/ups/idao/IVehiculoDAO.java
@@ -0,0 +1,18 @@
+package ec.edu.ups.idao;
+
+import ec.edu.ups.modelo.Vehiculo;
+import java.util.Map;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface IVehiculoDAO {
+    
+    public void create(Vehiculo vehiculo);
+    public Vehiculo read(String placa);
+    public void update(Vehiculo vehiculo);
+    public void delite(Vehiculo vehiculo);
+    public Map<String, Vehiculo> findAll();
+    
+}
